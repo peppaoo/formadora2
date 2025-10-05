9c211558f024d94398d9d08ba3666319e74c729f
peppaoo <lucascvgomes15@gmail.com>
2025-10-05 16:41:18 -0300

    att de mais uma pagina

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,25 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
   },
   {
+    path: 'home',
+    loadComponent: () =>
+      import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
     path: 'cep-consulta',
-    loadComponent: () => import('./cep-consulta/cep-consulta.page').then( m => m.CepConsultaPage)
+    loadComponent: () =>
+      import('./cep-consulta/cep-consulta.page').then(
+        (m) => m.CepConsultaPage
+      ),
+  },
+  {
+    path: 'card',
+    loadComponent: () =>
+      import('./card/card.page').then((m) => m.CardPage),
   },
 ];
---
+++ src/app/card/card.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.page.html',
+  styleUrls: ['./card.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, RouterModule],
+})
+export class CardPage {}
